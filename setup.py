5e38a50582a86c4e98152e3139c9afd0ef806e83
Zain Memon <zain@inzain.net>
2013-12-01 02:44:05 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='pyr',
-    version='0.3.1',
+    version='0.3.2',
     description='A nicer REPL for Python.',
     author='Zain Memon',
     author_email='zain@inzain.net',
