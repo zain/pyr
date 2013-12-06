04423e734a5f09909725bb53c3d16768b0ae3225
Zain Memon <zain@inzain.net>
2013-12-06 02:57:59 -0800

    Add the option to specify an initfile

--- pyr/shell.py
+++ pyr/shell.py
@@ -1,4 +1,4 @@
-import os
+import argparse, os
 
 from pyr.console import PyrConsole
 from pyr.defaults import default_config
@@ -11,9 +11,21 @@ def main(config=None):
         if os.path.exists(config_path):
             execfile(config_path, config)
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--init", dest="init_filename", required=False,
+        help="input file with two matrices", metavar="FILE")
+    args = parser.parse_args()
+
+    initfile = None
+    if args.init_filename:
+        if not os.path.exists(args.init_filename):
+            parser.error("Init file %s does not exist" % args.init_filename)
+        else:
+            initfile = open(args.init_filename, 'r')
+
     config = dict(default_config.items() + config.items())
-    console = PyrConsole(pygments_style=config.get('pygments_style'))
-    console.interact(banner=config.get('banner'))
+    console = PyrConsole(pygments_style=config.get('pygments_style'), initfile=initfile, banner=config.get('banner'))
+    console.interact()
 
 
 if __name__ == '__main__':
